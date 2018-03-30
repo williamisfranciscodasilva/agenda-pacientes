4c110fe6057887e50faf85bfc2c55ea7be1d262d
Williamis Silva <williamisepaula@gmail.com>
2018-03-29 23:10:49 -0300

    atualizando index.html e calcula-imc.js usando DOM, variáveis, objetos, e arrays

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,16 +1,28 @@
 //peso dividido por altura ao quadrado, ou seja, peso / (altura*altura)
-var tdPeso = document.getElementById("peso-2");
-var tdAltura = document.getElementById("altura-2");
 
-var paciente = {peso: tdPeso.textContent, altura: tdAltura.textContent};
+var trsPacientes = document.getElementsByClassName("paciente");
+
+var posicaoAtual = 0;
+while(posicaoAtual <= trsPacientes.length == 1){
+
+    var pacienteTr = trsPacientes[posicaoAtual];
+    
+    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+    
+    
+    var paciente = {nome : tdNome.textContent, peso: tdPeso.textContent, altura: tdAltura.textContent};
 
 if(paciente.altura != 0) {
     var imc = paciente.peso / (paciente.altura * paciente.altura);
-    var tdImc = document.getElementById("imc-2");
+    var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
     tdImc.textContent = imc;
 
     console.log(imc);
 }
 else{
-    console.log("Não executarei, porque a altura é igual a 0");
+    console.log("Não posso dividir");
+}
+    posicaoAtual++;
 }
