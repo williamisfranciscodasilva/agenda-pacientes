33309c6dfe32c812c06b52d2241ae268bf48841c
Williamis Silva <williamisepaula@gmail.com>
2018-04-02 06:40:13 -0300

    alterando o local da função no calcula-imc.js

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,13 +1,5 @@
 //IMC = peso dividido por altura ao quadrado, ou seja, peso / (altura*altura)
-function calculaImc(paciente){
-    if(paciente.altura != 0) {
-        var imc = paciente.peso / (paciente.altura * paciente.altura);
-        return imc;
-    }
-    else{
-        console.log("Não posso dividir");
-    }
-}
+
 var trsPacientes = document.getElementsByClassName("paciente");
 
 for(posicaoAtual = 0; posicaoAtual <= trsPacientes.length == 1; posicaoAtual++){
@@ -18,9 +10,20 @@ for(posicaoAtual = 0; posicaoAtual <= trsPacientes.length == 1; posicaoAtual++){
     var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
     var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
     
-    var pacienteAtual = {nome : tdNome.textContent, peso: tdPeso.textContent, altura: tdAltura.textContent};
+    var pacienteAtual = {nome : tdNome.textContent,
+                         peso: tdPeso.textContent,
+                         altura: tdAltura.textContent,
+                         pegaImc: function(){
+                            if(this.altura != 0) {
+                                var imc = this.peso / (this.altura * this.altura);
+                                return imc;
+                            }
+                            else{
+                                console.log("Não posso dividir");
+                            }
+                         }};
 
-    var imc = calculaImc(pacienteAtual);
+    var imc = pacienteAtual.pegaImc(pacienteAtual);
 
     var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
     tdImc.textContent = imc;
